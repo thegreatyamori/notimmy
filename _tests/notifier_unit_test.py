af9881d7214624c28ca0c74dcdaa6e1b3c9e593e
thegreatyamori <tod.jerson.morocho@gmail.com>
2021-12-30 23:25:53 -0500

    add tests

---
+++ _tests/notifier_unit_test.py
@@ -0,0 +1,24 @@
+import pytest
+import src.notifier_service as notifier_service
+
+
+@pytest.fixture()
+def mock_dependencies(mocker):
+    return {
+        'notify': mocker.patch('src.notifier_service.Notify')
+    }
+
+
+def test__show_simple_notification__when_display_method_is_called(mock_dependencies):
+    params = {
+        'title': 'hello',
+        'message': 'world',
+    }
+
+    notifier = notifier_service.NotifierService(**params)
+    notifier.display()
+
+    assert mock_dependencies['notify'].return_value.title == params['title']
+    assert mock_dependencies['notify'].return_value.message == params['message']
+    mock_dependencies['notify'].assert_called()
+    mock_dependencies['notify'].return_value.send.assert_called()
---
+++ _tests/request_unit_test.py
@@ -0,0 +1,2 @@
+def test_request_calls_binance_p2p_api():
+    assert 1 == 2
